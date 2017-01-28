49c32099af1511e88e745353a20dfd81c3bb011d
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-28 15:01:47 +1000

    Added support for plugin icons

--- peek_client/backend/home/HomeModelTupleProvider.py
+++ peek_client/backend/home/HomeModelTupleProvider.py
@@ -16,15 +16,18 @@ class PluginAppTileTuple(Tuple):
     name = TupleField(comment="The name of the plugin, EG plugin_noop")
     title = TupleField(comment="The title of the plugin, EG No Op")
     resourcePath = TupleField(comment="The resource path of the plugin")
+    pluginIconPath = TupleField(comment="The resource of the plugin icon")
 
 
 class HomeAppTileTupleProvider(TuplesProviderABC):
 
     def makeVortexMsg(self, filt: dict, tupleSelector: TupleSelector) -> bytes:
         tuples = []
-        for name, title, path in PeekPlatformConfig.pluginLoader.pluginFrontendTitleUrls:
+        for name, title, path, iconPath \
+                in PeekPlatformConfig.pluginLoader.pluginFrontendTitleUrls:
             tuples.append(PluginAppTileTuple(name=name,
                                              title=title,
-                                             resourcePath=path))
+                                             resourcePath=path,
+                                             pluginIconPath=iconPath))
 
         return Payload(filt=filt, tuples=tuples).toVortexMsg()
