675b4a11611c06622a33b70295b30b36eacd112b
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-01-09 10:15:13 +1000

    Updated to version 0.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 pip_package_name = "peek-client"
 py_package_name = "peek_client"
 
-package_version = '0.8.0'
+package_version = '0.8.1'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
