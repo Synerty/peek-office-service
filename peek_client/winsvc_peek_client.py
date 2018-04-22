6472e243fbc5898b5135e3506a479d4adda7c982
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-22 13:54:34 +1000

    Updated winsvc SvcDoRun methods
    ORMOB-600

--- peek_client/winsvc_peek_client.py
+++ peek_client/winsvc_peek_client.py
@@ -1,29 +1,18 @@
-import platform
+import logging
 
-import peek_client
-from peek_platform.sw_install.PeekSwInstallManagerABC import PeekSwInstallManagerABC
-from peek_platform.util.LogUtil import setupPeekLogger
-
-try:
-    import win32serviceutil
-    import win32service
-    import win32event
-except ImportError as e:
-    if platform.system() is "Windows":
-        raise
-
+import win32service
+import win32serviceutil
 from twisted.internet import reactor
 
-from peek_client import run_peek_client
+logger = logging.getLogger(__name__)
 
 
 class PeekSvc(win32serviceutil.ServiceFramework):
     _svc_name_ = "peek_client"
-    _svc_display_name_ = "Peek Client " + peek_client.__version__
+    _svc_display_name_ = "Peek Client "  # + peek_client.__version__
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
-        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
 
         reactor.addSystemEventTrigger('after', 'shutdown', self._notifyOfStop)
 
@@ -35,31 +24,33 @@ class PeekSvc(win32serviceutil.ServiceFramework):
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.hWaitStop)
         reactor.callFromThread(reactor.stop)
 
     def SvcDoRun(self):
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
-        reactor.callLater(1, self._notifyOfStart)
-        run_peek_client.main()
-        reactor.run()
+        try:
 
+            # Patch the restart method for windows services
+            class _Restart:
+                def _restartProcess(self):
+                    reactor.callFromThread(reactor.stop)
 
-# Patch the restart method for windows services
-class _Restart:
-    def _restartProcess(self):
-        reactor.callFromThread(reactor.stop)
+            # Patch the restart call for windows
+            from peek_platform.sw_install.PeekSwInstallManagerABC import \
+                PeekSwInstallManagerABC
+            PeekSwInstallManagerABC.restartProcess = _Restart._restartProcess
 
+            reactor.callLater(1, self._notifyOfStart)
 
-# Patch the restart call for windows
-PeekSwInstallManagerABC.restartProcess = _Restart._restartProcess
+            from peek_client import run_peek_client
+            run_peek_client.main()
 
-
-# end patch
+        except Exception as e:
+            logger.exception(e)
+            raise
 
 
 def main():
-    setupPeekLogger(PeekSvc._svc_name_)
     win32serviceutil.HandleCommandLine(PeekSvc)
 
 
